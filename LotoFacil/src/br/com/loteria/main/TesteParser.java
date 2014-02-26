3ea75f295ee90ad82aefe8ecfb01792aa484f326
cust <eversont@gmail.com>
2014-02-26 00:08:00 -0300

    funcionando parser

--- LotoFacil/src/br/com/loteria/main/TesteParser.java
+++ LotoFacil/src/br/com/loteria/main/TesteParser.java
@@ -1,6 +1,7 @@
 package br.com.loteria.main;
 
 import java.io.File;
+import java.util.Iterator;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -14,15 +15,29 @@ public class TesteParser {
 			File input = new File("/tmp/D_LOTFAC.HTM");
 			Document doc = Jsoup.parse(input, "UTF-8", "http://example.com/");
 
-			Element content = doc.getElementById("content");
-			Elements links = content.getElementsByTag("table");
-			for (Element link : links) {
-				String linkHref = link.attr("td");
-				String linkText = link.text();
-				System.out.println(linkText);
+			//Elements linhass = doc.getElementsByTag("tr");
+			//for (Element linha : linhass) {
+				//String linkHref = linha.attr("td");
+				//String linkText = linha.text();
+				// System.out.println(linkText);
+			//}
+
+			for (Element tabela : doc.select("table")) {
+				for (Element linhas : tabela.select("tr")) {
+					Elements colunas = linhas.select("td");
+					//if (colunas.size() > 6) {
+						// System.out.println(colunas.get(0).text() + ":"+ colunas.get(1).text());
+					//}
+					String str = "";
+					for (Integer i = 0; i < colunas.size(); i++) {
+						str = str + colunas.get(i).text() + ":";
+					}
+					System.out.println(str);
+				}
 			}
 
 		} catch (Exception e) {
+			
 		}
 
 	}
