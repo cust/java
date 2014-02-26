38983734f936826ed8baa3c098dfcf66fb001823
cust <eversont@gmail.com>
2014-02-25 22:51:28 -0300

    primeiro commit importante

---
+++ LotoFacil/src/br/com/loteria/model/Loteria.java
@@ -0,0 +1,7 @@
+package br.com.loteria.model;
+
+public class Loteria {
+
+	private String urlResultados = "";
+
+}
---
+++ LotoFacil/src/br/com/loteria/model/LotoFacil.java
@@ -0,0 +1,12 @@
+package br.com.loteria.model;
+
+public class LotoFacil extends Loteria {
+
+	// http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_lotfac.zip
+
+	public LotoFacil() {
+		super();
+
+	}
+
+}
