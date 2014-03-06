656065adc76356dd7b49b20d45e9ec22ef015e19
cust <eversont@gmail.com>
2014-03-06 13:37:47 -0300

    sobrescrita de nomes

--- HibernateComPostgres/src/tabelas/Tabela01.java
+++ HibernateComPostgres/src/tabelas/Tabela01.java
@@ -3,6 +3,7 @@ package tabelas;
 import javax.persistence.Entity;
 
 @Entity
+@AttributeOverride(name = "id", column = @Column(name = "id_tabela01", nullable = false)
 public class Tabela01 extends Tabela {
 
 }
