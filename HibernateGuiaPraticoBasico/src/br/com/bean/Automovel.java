450bec99cf109efdaf319ef4d3f33d1ac8751d45
cust <eversont@gmail.com>
2014-03-10 23:37:45 -0300

    novo exemplo

---
+++ HibernateGuiaPraticoBasico/src/br/com/bean/Automovel.java
@@ -0,0 +1,41 @@
+package br.com.bean;
+
+public class Automovel {
+	private int id;
+	private String nome;
+	private int ano;
+	private String fabricantenome;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+	public String getFabricantenome() {
+		return fabricantenome;
+	}
+
+	public void setFabricantenome(String fabricantenome) {
+		this.fabricantenome = fabricantenome;
+	}
+
+}
---
+++ HibernateGuiaPraticoBasico/src/br/com/dao/DAO.java
@@ -0,0 +1,41 @@
+package br.com.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DAO {
+	private static final ThreadLocal threadlocal = new ThreadLocal();
+
+	private static final SessionFactory sessionfactory = new Configuration().configure().buildSessionFactory();
+
+	public DAO() {
+
+	}
+
+	public static Session getSession() {
+		Session session = (Session) threadlocal.get();
+		if (session == null) {
+			session = sessionfactory.openSession();
+			threadlocal.set(session);
+		}
+		return session;
+	}
+
+	public void begin() {
+		getSession().beginTransaction();
+	}
+
+	public void commit() {
+		getSession().getTransaction().commit();
+	}
+
+	public void rollback() {
+		getSession().getTransaction().rollback();
+	}
+
+	public void close() {
+		getSession().clear();
+		threadlocal.set(null);
+	}
+}
